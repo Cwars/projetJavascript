c9f40e63b2e48a4a89f2f070eb6ad89acadcd04e
Alexandre Ting <alexandre.ting@ymail.com>
2017-06-23 17:57:09 +0200

    Nearly done
    Juste les multiples clics à voir et les puces

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <title>Description de la page</title>
-    <meta name ="description" content="descritption de la page">
+    <title>Slideshow</title>
+    <meta name ="Slideshow" content="Slideshow">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -16,20 +16,43 @@
                 $parsed_json = json_decode($json);
 
                     foreach ($parsed_json as $id => $obj) {
-                        echo "<li>";
-                        echo "<img src='".$obj->{'url'}."' alt='".$obj->{'desc'}."'>";
+                        echo "<li id='slider'>";
+                        echo "<img src='".$obj->{'url'}."' alt='".$obj->{'title'}."'>";
                         echo "</li>";
                     }
                 ?>
             </div>
         </ul>
+
+        <ul id="slide-for-text">
+            <div id="rail-text">
+                <?php
+                foreach ($parsed_json as $id => $obj) {
+                    echo "<li id='sliderTexte'>";
+                    echo "<p>".$obj->{'desc'}."<p/>";
+                    echo "</li>";
+                }
+                ?>
+            </div>
+        </ul>
 </div>
 
-<button id="previous">Précédent</button>
-<button id="play">lecture/Pause</button>
-<button id="next">Suivant</button>
+<!--<div id="button-dots">
+    <button class="active" data-id="0" id="dots">0</button>
+    <button data-id="1" id="dots">1</button>
+    <button data-id="2" id="dots">2</button>
+    <button data-id="3" id="dots">3</button>
+</div>-->
+
+<div id="button-control">
+    <button id="previous">Précédent</button>
+    <button id="play">Lecture</button>
+    <button id="pause">Pause</button>
+    <button id="next">Suivant</button>
+</div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="js/script.js"></script>
+
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,102 +1,180 @@
 // Give an id to every img in the slideshow>Rail class
 $(document).ready(function() {
-
 	start();
-	createDots();
 	mySlider();
-	// $( "#slideshow img" ).attr( "id", function( arr ) {
-	// 	return arr;
-	// });
-
 });
 
 var containerName = "#slideshow";
 var liste = "#slide-for";
 var railname = "#rail";
-
 var container = $(containerName);
 var rail = container.children(liste).children(railname);
+
+var listeTexte = "#slide-for-text";
+var railnameTexte = "#rail-text";
+var railTexte = container.children(listeTexte).children(railnameTexte);
+
+var dot = "#button-dots";
+var containerDots = $(dot);
+
+var button ="#button-control";
+var containerButton = $(button);
+
 var imgWidth = 800;
 var leftBase = -Math.abs(imgWidth);
-var speed = 1000;
+var speedSlide = 1000;
+var speedPlay = 3000;
 
 var nbImg;
-
+var idloop;
 
 function mySlider(){
 
-	count();
+    containerButton.children("#previous").click(precedent);
 
-	container.siblings("button:first").click(precedent);
+    containerButton.children("#next").click(suivant);
 
-	container.siblings("button:last").click(suivant);
+    containerButton.children("#play").click(play);
 
-	container.siblings("button").eq(1).click(function () {
-	    rail.children( "img" ).toggle();
-	});
+    containerButton.children("#pause").click(pause);
 
+}
+
+function start(){
+    count();
+    createButtonDots();
+
+    rail.css( "left", leftBase);
+    railTexte.children("li:not(:first)").hide();
+
+    var sliderWidth = imgWidth * nbImg;
+    rail.css( "width", sliderWidth);
+
+    containerButton.children("#pause").hide();
+
+    startObj(rail,railname,"li:last");
+    startObj(railTexte,railnameTexte,"li:last");
+
+}
+function startObj(obj,id,item){
+    obj.children("li").attr( "data-id", function( arr ) {
+        return arr;
+    });
+
+    startclone(obj,id,item);
+
+    obj.children("[data-id=0]").addClass( "active" );
+}
+
+function startclone(obj,id,item){
+    obj.children( item ).clone().prependTo(id);
+    obj.children( item ).remove();
 }
 
 function suivant(){
 	    left = leftBase-imgWidth;
 	    rail.animate({
-			left: left,
-			}, speed, function() {
-			// Animation complete.
+			left: left
+			}, speedSlide, function() {
+			// Image animation complete.
 				$(this).children( "li:first" ).clone().appendTo( railname );
 			    $(this).children( "li:first" ).remove();
 			    $(this).css( "left", leftBase+'px');
+
+				var futurAct =  $(this).children(".active").next().attr('data-id');
+				$(this).children("[class = active]").removeClass("active");
+				$(this).children("[data-id="+futurAct+"]").addClass( "active" );
+
+				//Texte animation
+				railTexte.children( "li:first" ).clone().appendTo( railnameTexte );
+				railTexte.children( "li:first" ).remove();
+
+				var futurActText = railTexte.children(".active").next().attr('data-id');
+				railTexte.children("[class = active]").fadeOut().removeClass("active");
+				railTexte.children("[data-id="+futurActText+"]").addClass( "active" ).fadeIn();
 			}
 		);
-	}
+}
 
 function precedent(){
 	    left = 0;
 	    rail.animate({
-			left: left,
-			}, speed, function() {
+			left: left
+			}, speedSlide, function() {
 			// Animation complete.
 				$(this).children( "li:last" ).clone().prependTo( railname );
 			    $(this).children( "li:last" ).remove();
 			    $(this).css( "left", leftBase+'px');
+
+				var futurAct = $(this).children(".active").prev().attr('data-id');
+				$(this).children("[class = active]").removeClass("active");
+				$(this).children("[data-id="+futurAct+"]").addClass( "active" );
 			}
 		);
+    railTexte.animate(
+        speedSlide, function() {
+            //Texte animation
+            $(this).children("li:last").clone().prependTo(railnameTexte);
+            $(this).children("li:last").remove();
+
+            var futurActText = $(this).children(".active").prev().attr('data-id');
+            $(this).children("[class = active]").hide().removeClass("active");
+            $(this).children("[data-id=" + futurActText + "]").addClass("active").show();
+        });
+
 	}
 
-function start(){
-	count();
-	rail.children( "li:last" ).clone().prependTo( railname );
-	rail.children( "li:last" ).remove();
+function play() {
+    idloop = setInterval(suivant, speedPlay);
+    containerButton.children("#play").hide();
+    containerButton.children("#pause").show();
+}
 
-	rail.css( "left", leftBase);
-
-	var sliderWidth = imgWidth * nbImg;
-	rail.css( "width", sliderWidth);
-
+function pause(){
+    clearInterval(idloop);
+    containerButton.children("#pause").hide();
+    containerButton.children("#play").show();
 }
 
 function count() {
 	nbImg = rail.children( "li" ).length;
 }
 
-function createDots() {
-	var ul = document.createElement("ul");
-	ul.setAttribute("id", "slide-nav");
+function createButtonDots() {
+    var div = document.createElement("div");
+    div.setAttribute("id", "button-dots");
 
 	for(i=0 ; i<nbImg ; i++){
-	var li = document.createElement("li");
-        if(i==0){
-            li.setAttribute("id", "active");
-        }
-	li.setAttribute("class", "slide");
-	var li_content = document.createTextNode(i);
-	li.appendChild(li_content);
-	ul.appendChild(li);
-	}
-	var div2 = document.getElementById("previous");
+        var button = document.createElement("button");
+        button.setAttribute( "data-id", i);
+        button.setAttribute("id", "dots");
+		var button_content = document.createTextNode(i);
+		button.appendChild(button_content);
+        div.appendChild(button);
+        button.onclick = dotsAction;
+    }
+	var div2 = document.getElementById("button-control");
 	var parentDiv = div2.parentNode;
 
-	parentDiv.insertBefore(ul, div2);
+	parentDiv.insertBefore(div, div2);
+}
+
+function dotsAction(){
+	if($(this).attr('class') !== "active"){
+	   var dotsActive = rail.children(".active").attr('data-id');
+	   var dotsClick = $(this).attr('data-id');
+
+	   var diff = dotsClick - dotsActive;
+	   if (diff>0){
+			for(var i = 0; i< diff; i++){
+				suivant();
+			}
+	   }else{
+           for( i = 0; i> diff; i--){
+           		precedent();
+           }
+	   }
+    }
 }
 
 
